5f889e310c5a92929a34dc6e7c9e25d0a10cae88
Martin Shetty <1972005+martukas@users.noreply.github.com>
2022-04-21 01:04:50 -0700

    "added empty page; updates #5"

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import MyNavbar from './pages/navbar';
 import DataFileTable from './pages/data-file-table';
 import UploadFile from './pages/upload-file';
-import MyNavbar from './pages/navbar';
+import DataSet from './pages/dataset';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<DataFileTable />} />
           <Route path="/upload-file" element={<UploadFile />} />
+          <Route path="/dataset" element={<DataSet />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/client/pages/dataset.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DataSet() {
+  return (
+    <div>
+      Nothing to see here
+    </div>
+  );
+}
+
+export default DataSet;
